9feafa3f4efbd475bbec166eaa3c9d00489d3424
shubham12-coder <shubham.xentumbrandsolutions@gmail.com>
2026-08-11 14:21:30 +0530

    Fix gifting gallery images

--- app/projects/merchandising-gifting/page.tsx
+++ app/projects/merchandising-gifting/page.tsx
@@ -10,7 +10,7 @@ const images = [
   '/images/projects/gifting/q1.jpeg',
   '/images/projects/gifting/q2.jpeg',
   '/images/projects/gifting/h1.jpeg',
-  '/images/projects/gifting/q3.jpeg',
+  '/images/projects/gifting/q4.png',
   '/images/projects/gifting/g5.jpeg',
   '/images/projects/gifting/g6.jpeg',
 ]
